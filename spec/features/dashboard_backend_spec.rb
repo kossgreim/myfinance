538fcdeaf74981958e42c7a1ec36bc3cdc2a3302
swtnext <swtnext@gmail.com>
2015-05-08 08:57:14 +0200

    add some tests

--- spec/features/dashboard_backend_spec.rb
+++ spec/features/dashboard_backend_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 describe "Dashboard page", :type => :feature do
 
-  xit "Valid sign in link" do
+  context "when signed in" do
+    it "has Log out link" do
+      user = create(:user)
+      visit new_user_session_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+      expect(page).to have_link('Log out')
+    end
   end
 end
 
--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 describe "frontend index page", :type => :feature do
 
-  it "Valid sign in link" do
+  it "sign in link" do
     visit homepage_path
-    click_link 'Sign in'
-    expect(page).to have_content('Sign in')
-
+    expect(page).to have_link "Sign in"
   end
 end
 
